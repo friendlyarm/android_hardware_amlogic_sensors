5e1672f9e487b4c12b715268e4c7a3ce7f65501d
hmz007 <hmz007@gmail.com>
2016-12-01 13:55:39 +0800

    LOCAL_MODULE: `amlogic' ==> $(TARGET_DEVICE)
    Signed-off-by: hmz007 <hmz007@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,8 +1,8 @@
 ifneq ($(BOARD_USES_AML_SENSOR_HAL),false)
-LOCAL_PATH:=$(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := sensors.amlogic
+LOCAL_MODULE := sensors.$(TARGET_DEVICE)
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -11,8 +11,6 @@ LOCAL_MULTILIB := 32
 LOCAL_SRC_FILES := sensors.amlogic.so
 include $(BUILD_PREBUILT)
 
-
 #libraries to copy
 
 endif
-
